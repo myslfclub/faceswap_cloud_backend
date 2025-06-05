32800e2f006e0bb5fa57f97a6fb92fc1416cda5b
myslfclub <myslfclub@gmail.com>
2025-06-05 19:34:56 +0200

    Delete facefusion_wrapper_video.py

--- facefusion_wrapper_video.py
+++
@@ -1,4 +0,0 @@
-def swap_video_faces(video_path, face_path, output_path, resolution):
-    # Simule le traitement (remplace ceci par l'appel à FaceFusion réel)
-    import shutil
-    shutil.copy(video_path, output_path)
